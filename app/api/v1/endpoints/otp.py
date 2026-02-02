4804a4622b30398d7df13607e16e33cbc552ab65
Jeremiah Ojonuba <ojonuba93@gmail.com>
2026-02-02 05:36:55 +0100

    feat(mq): implement rabbitmq publisher service

--- app/api/v1/endpoints/otp.py
+++ app/api/v1/endpoints/otp.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from redis import Redis
-from app.schemas.otp import OTPRequest, OTPVerify
+from app.schemas.otp import OTPMessage, OTPRequest, OTPVerify
 from app.utils import generate_otp, hash_otp
 from app.db.redis import get_redis
+from app.services.rabbitmq import RabbitMQService
+
 
 router = APIRouter()
 
-@router.post("/generate-otp", status_code=201)
+@router.post("/generate-otp", status_code=status.HTTP_202_ACCEPTED)
 async def generate_otp_endpoint(payload: OTPRequest, r: Redis= Depends(get_redis)):
     identifier = payload.email
     otp_code = generate_otp()
@@ -16,8 +18,22 @@ async def generate_otp_endpoint(payload: OTPRequest, r: Redis= Depends(get_redis
         r.setex(redis_key, 300, hashed_otp)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Redis error: {str(e)}")
+    
+    # Publish OTP to RabbitMQ for asynchronous processing (e.g., sending email)
+    # In a massive scale app, we might inject this service too, 
+    # but instantiating it here is fine for now.
+    mq_service = RabbitMQService()
+    
+    message = OTPMessage(
+        email=identifier,
+        otp_code=otp_code,
+        ttl=300
+    )
+    try:
+        mq_service.publish_otp(message)
+    except Exception as e:
+        r.delete(f'otp:{identifier}')  # Rollback OTP storage on failure
+        raise HTTPException(status_code=500, detail=f"Messaging error: {str(e)}")
     return {
-        "message": "OTP generated successfully",
-        "dev_only_code": otp_code,
-        "ttl": 300
+        "message": "OTP generated successfully"
     }
